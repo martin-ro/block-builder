8bc17864656245e528f107c819f1058822a30f62
Martin Ro <martin-ro@users.noreply.github.com>
2024-08-15 14:18:18 +0800

    wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'blocks',
     ];
 
     /**
@@ -42,6 +43,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'blocks' => 'array',
         ];
     }
 }
